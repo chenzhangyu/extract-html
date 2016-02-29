f25293b18d7c3ae89ec7c9a2eb8fc5d8513b758f
chenzhangyu <chenzhangyu.hust.cn@gmail.com>
2016-02-29 13:23:10 +0800

    Add config.conf

--- app.py
+++ app.py
@@ -6,13 +6,15 @@ import tornado.web
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
+from tornado.options import options
 
+import settings as config  # noqa
 import handler
 
 
 settings = {
-    "debug": True,
-    "port": 24300
+    "debug": options.debug,
+    "port": options.port
 }
 
 
@@ -25,15 +27,15 @@ def main():
     tornado.options.parse_command_line()
     application = tornado.web.Application(urls, **settings)
     server = tornado.httpserver.HTTPServer(application)
-    logging.info("start on 127.0.0.1:{}".format(settings["port"]))
-    server.listen(settings["port"])
+    logging.info("start on 127.0.0.1:{}".format(options.port))
+    server.listen(options.port)
     server.start()
     tornado.ioloop.IOLoop.instance().start()
 
 
 if __name__ == "__main__":
     logging.basicConfig(
-        filename="server.log",
+        filename=options.log_file,
         level=logging.DEBUG,
         format="[%(name)s][%(levelname)s][%(asctime)s]: %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S"
--- setttings.py
+++
@@ -1,3 +0,0 @@
-# coding=utf8
-
-from tornado.options import parse_config_file
